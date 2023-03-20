deb3761127183b1000d63a8195d8b359caacfd31
Yaroslav <yomelianenkowork@gmail.com>
2023-03-20 08:43:38 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Counter } from './components/Counter';
+import { Header, Layout } from './components/Layout';
+// import { Banner } from './components/Banner';
+// import { UsersList } from './components/Users';
+
+export const App = () => {
+  return (
+    <Layout>
+      <Header title="Hello world!" />
+
+      <Counter />
+      {/* <Banner /> */}
+      {/* <UsersList /> */}
+    </Layout>
+  );
+};
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+import { IoCashOutline } from "react-icons/io5";
+
+import { BannerItem } from "./BannerItem";
+// import { Modal } from '../Modal';
+// import { BannerModal } from './BannerModal';
+
+const TEXT =
+  "Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.";
+
+export const Banner = () => {
+  return (
+    <>
+      <div className="row mb-5 p-5 row-cols-2 bg-light">
+        <BannerItem title="Featured title" text={TEXT}>
+          <IoCashOutline />
+        </BannerItem>
+      </div>
+
+      {/* <Modal>
+        <BannerModal />
+      </Modal> */}
+    </>
+  );
+};
